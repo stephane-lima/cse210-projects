fd2e2107106b54c412abc24b12921835f9d73f43
stephane-lima <ggabriellopess99999@gmail.com>
2023-01-07 19:54:05 -0100

    C# Prep 2 - Conditionals Added

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,70 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradeInt = int.Parse(grade);
+        string letter = "";
+        string sign = "";
+
+        if (gradeInt >= 90 && gradeInt <= 100)
+        {
+            letter = "A";
+        }
+        else if (gradeInt >= 80 && gradeInt < 90)
+        {
+            letter = "B";
+        }
+        else if (gradeInt >= 70 && gradeInt < 80)
+        {
+            letter = "C";
+        }
+        else if (gradeInt >= 60 && gradeInt <70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        
+        if (gradeInt % 10 >= 7)
+        {
+            if (letter == "A" || letter == "F")
+            {
+                sign = "";
+            }
+            else
+            {
+                sign = "+";
+            }
+        }
+        else if (gradeInt % 10 <= 3)
+        {
+            if (letter == "F")
+            {
+                sign = "";
+            }
+            else
+            {
+                sign = "-";
+            }
+        }
+        else 
+        {
+            sign = "";
+        }
+        
+        Console.WriteLine($"Your grade letter is {letter}{sign}");
+        
+        if (gradeInt >= 70 && gradeInt <= 100)
+        {
+            Console.WriteLine("You passed the class");
+        }
+        else if (gradeInt < 70)
+        {
+            Console.WriteLine("You failed the Class");
+        }
     }
 }

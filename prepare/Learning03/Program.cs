205d94401f05f85b7ff590a5aabf2784a9c032dd
stephane-lima <ggabriellopess99999@gmail.com>
2023-02-12 05:03:39 -0100

    Create Files

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+        
+        Fraction fraction1 = new Fraction();
+
+        // Fraction f = new Fraction();
+        // f.SetTop(5);
+        // Console.WriteLine(f.GetTop());
+
     }
 }

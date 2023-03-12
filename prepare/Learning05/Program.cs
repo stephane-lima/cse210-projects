4c204ebafb3b54f78828ab86d335c43b6fea46cb
stephane-lima <ggabriellopess99999@gmail.com>
2023-03-12 02:19:26 -0100

    Week 9 Prepare Activity Added

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
+        // Console.WriteLine(s1.GetColor());
+        // Console.WriteLine(s1.GetArea());
+
+        Rectangle s2 = new Rectangle("Blue", 4, 5);
+        shapes.Add(s2);
+        // Console.WriteLine(s2.GetColor());
+        // Console.WriteLine(s2.GetArea());
+
+        Circle s3 = new Circle("Green", 6);
+        shapes.Add(s3);
+        // Console.WriteLine(s3.GetColor());
+        // Console.WriteLine(s3.GetArea());
+
+        foreach (Shape s in shapes)
+        {
+           string color = s.GetColor();
+           double area = s.GetArea();
+
+           Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
